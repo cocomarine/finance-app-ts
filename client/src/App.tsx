796dca2b856c6778ee7a9116a0dadd22de792f66
HJ Kang <hkang07@gmail.com>
2023-05-03 14:45:20 +0100

    project setup and navbar updated

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "@/scenes/navbar";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []); // createTheme from mui, use with themeSettings
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* to reset CSS to default */}
+          <CssBaseline /> 
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>predictions page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,14 @@
+// expaning theme setting 
+// eslint-disable-next-line
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteColor {
+    [key: number]: string;
+  }
+
+  // extending tertiary to have a PaletteColor as well
+  interface Palette {
+    tertiary: PaletteColor;
+  }
+}
