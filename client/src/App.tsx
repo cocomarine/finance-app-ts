5a548e3f500bb7615f6cd128dc30153d82ae0b6c
HJ Kang <hkang07@gmail.com>
2023-05-04 10:38:05 +0100

    grid template added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []); // createTheme from mui, use with themeSettings
@@ -16,7 +17,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <NavBar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>predictions page</div>} />
             </Routes>
           </Box>
