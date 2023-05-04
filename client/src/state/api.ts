0a132b42d31f66f977c57b6e61715624e3d4e0f6
HJ Kang <hkang07@gmail.com>
2023-05-04 22:52:59 +0100

    created dashboard templates and redux toolkit boilerplate

---
+++ client/src/state/api.ts
@@ -0,0 +1,17 @@
+// creating redux toolkit boilerplate
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis"],
+  endpoints: (build) => ({
+    getKpis: build.query<void, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"]
+    })
+  })
+})
+
+// useGetKpisQuery hook
+export const { useGetKpisQuery } = api;
