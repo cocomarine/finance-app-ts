c9b0ba96ea54fe4b31426c621989235d16d28ddc
HJ Kang <hkang07@gmail.com>
2023-05-06 22:05:41 +0100

    created server and kpi models

---
+++ server/models/KPI.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+
+const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const dailySchema = new Schema(
+  {
+    date: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    }
+  },
+  { toJSON: { getters: true }} 
+)
+
+const monthSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    operationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    nonOperationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    }
+  },
+  { toJSON: { getters: true }} // to use 'get' property
+)
+
+const KPISchema = new Schema(
+  {
+    titalProfit: {
+      type: mongoose.types.Currency,
+      currency: "USD",
+      get: (v) => v / 100, // mongoose currency is x100 so we need to do this
+    },
+    totalRevenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    totalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expensesByCategory: {
+      type: Map,
+      of: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      }
+    },
+    monthlyData: [monthSchema],
+    dailyData: [dailySchema]
+  },
+  { timestamps: true, toJSON: { getters: true }} 
+);
+
+const KPI = mongoose.model("KPI", KPISchema);
+
+export default KPI;
