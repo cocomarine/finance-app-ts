e45e4a490a45cd7b708871e895e13ce38a6694e4
HJ Kang <hkang07@gmail.com>
2023-06-08 10:43:48 +0100

    unused prop removed from navbar

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -4,9 +4,7 @@ import CalcIcon from '@mui/icons-material/Calculate';
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
